49d494ab176922e62f5df5d24a3c8d2d15cb5ebb
Niharika_Ravela <109175491+Niharika-Ravela@users.noreply.github.com>
2024-07-28 02:23:23 -0500

    Update Weather_Observation_Station.sql

--- Easy/Weather_Observation_Station.sql
+++ Easy/Weather_Observation_Station.sql
@@ -1,6 +1,3 @@
-SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '[AEIOU]%' AND CITY NOT LIKE '%[AEIOU]';
-
-
 "Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
 Your result cannot contain duplicates.
 Input Format
@@ -10,4 +7,9 @@ The STATION table has
   State - Varchar
   Lat_N - Number
   Long_W - Number
-where LAT_N is the northern latitude and LONG_W is the western longitude.
+where LAT_N is the northern latitude and LONG_W is the western longitude."
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '[AEIOU]%' AND CITY NOT LIKE '%[AEIOU]';
+
+
+
